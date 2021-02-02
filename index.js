2c2e43956abfeee2cbc91acb336bfed2d0aab88b
NanniGmz <77912002+NanniGmz@users.noreply.github.com>
2021-02-02 20:39:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -130,8 +130,8 @@ async function starts() {
 				}
 			}
 
-			const botNumber = client.user.jid
-			const ownerNumber = ["6285892766102@s.whatsapp.net"] // replace this with your number
+			const botNumber = client.Abil.jid
+			const ownerNumber = ["6283891411268@s.whatsapp.net"] // replace this with your number
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
